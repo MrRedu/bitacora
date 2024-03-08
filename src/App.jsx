a5cbf9f6dd806bb6174a7e8e219b4fe9e9046ae9
Eduardo <ejrb1234@gmail.com>
2024-03-08 14:58:49 -0400

    Add: Accordion (Component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Section } from './components/section/Section'
+import { Accordion } from './components/accordion/Accordion'
+
 export const App = () => {
   return (
     <>
       <h1>{`</App>`}</h1>
+      <Section>
+        <Accordion />
+      </Section>
+
+      <Section isSecondary>
+        <h2>{`</Section>`}</h2>
+      </Section>
     </>
   )
 }
